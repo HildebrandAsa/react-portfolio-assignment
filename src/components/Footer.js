c276c43ba41661b133f9e9d5b9d3e6dfe677d257
Åsa Hildebrand <98025185+HildebrandAsa@users.noreply.github.com>
2022-05-16 13:39:00 +0200

    Fixed header and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,23 @@
 const Footer = () => {
 
   return (
-    <div className="footer">Its the footer</div>
+    <div className="footer">
+      <div className="footer-contact">
+        <h2>Åsa Hildebrand</h2>
+        <h3>asahildebrand@yahoo.com</h3>
+      </div>
+      <ul className="footer-list">
+      <li className="link">
+        <a href="https://github.com/HildebrandAsa" target="blank"><i class="fa-brands fa-github"></i></a>
+      </li>
+      <li className="link">
+        <a href="https://linkedin.com/in/åsa-hildebrand-45a446201" target="blank"><i class="fa-brands fa-linkedin-in"></i></a>
+      </li>
+      <li className="link">
+        <a href="https://twitter.com/asa_hildebrand" target="blank"><i class="fa-brands fa-twitter"></i></a>
+      </li>
+    </ul>
+    </div>
   );
 };
 
